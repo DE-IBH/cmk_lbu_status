bca3b2e95bd3a2cb6a552501d7eec21fca96171c
Philipp Kilian <kilian@ibh.de>
2018-09-14 17:13:15 +0200

    Update agent plugin

--- agent/lbu_status.py
+++ agent/lbu_status.py
@@ -4,25 +4,29 @@
 # Put this file into the PLUGINSDIR [$MK_LIBDIR/plugins] of the agent system
 
 import subprocess as sp
+import os
 from datetime import datetime as dt
 
-lbu_status_result_obj = sp.Popen("lbu status", shell=True)
-if lbu_status_result_obj.returncode != 0:
+try:
+    pipeout = sp.check_output(["lbu", "status"])
+except:
+    print "EXIT"
     exit()
 
 current_time = dt.now()
 
 print "<<<lbu_status>>>"
-
-for result in lbu_status_result_obj.stdout:
-    changemode = result.replace("\n", "").split(" ")[0]
+pipeout = pipeout.split("\n")[:-1]
+for result in pipeout:
+    print result
+    changemode = result.split(" ")[0]
     if len(result.split(" ")) > 2:
         splitted_result = result.replace("\n", "").split(" ")
         filepath = "/" + splitted_result[1]
         for result_part in splitted_result[2:]:
              filepath += " " + result_part
     else:
-        filepath = "/" + result.replace("\n", "").split(" ")[1]
+        filepath = "/" + result.split(" ")[1]
     if changemode == "A" or changemode == "U":
         changetime = dt.fromtimestamp(os.path.getmtime(filepath))
         seconds = (current_time - changetime).seconds
