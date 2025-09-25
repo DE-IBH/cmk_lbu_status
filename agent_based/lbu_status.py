d0244be9b8ffb89ef871da6e694a546b28be59e9
Philipp Kilian <kilian@ibh.de>
2025-09-25 12:54:51 +0200

    lbu_status: update for cmk API v2

--- agent_based/lbu_status.py
+++ agent_based/lbu_status.py
@@ -34,8 +34,7 @@ from typing import Any, List, Literal, Mapping
 from datetime import datetime as dt
 from datetime import timedelta as td
 
-from .agent_based_api.v1 import register, Result, Service, State
-from .agent_based_api.v1.type_defs import DiscoveryResult, InventoryResult, CheckResult, StringTable
+from cmk.agent_based.v2 import AgentSection, CheckPlugin, Metric, Result, Service, State, StringTable
 
 
 class LBU:
@@ -43,55 +42,64 @@ class LBU:
     mode: Literal["A", "U", "D"]
     path: str
 
-    def get_time_in_seconds(self) -> int:
-        return self.time.days * 86400 + self.time.seconds
-
-    def get_time_string(self) -> str:
-        return (dt.now() - self.time).strftime("%Y-%m-%d %H:%M:%S")
-
     def __init__(self, time: int, mode: Literal["A", "U", "D"], path: str):
         self.time = td(seconds=time)
         self.mode = mode
         self.path = path
 
+    def __str__(self):
+        return f"{self.mode} {self.get_time_in_seconds} {self.path}"
+
+    @property
+    def get_time_in_seconds(self) -> int:
+        return self.time.days * 86400 + self.time.seconds
+
+    @property
+    def get_time_string(self) -> str:
+        return (dt.now() - self.time).strftime("%Y-%m-%d %H:%M:%S")
+
 
 def check_lbu_status(params: Mapping[str, Any], section: List[LBU]) -> CheckResult:
-    warn, crit = params.get("levels", (1800, 3600))
+    warn = params["warning_seconds"]
+    crit = params["critical_seconds"]
     max_time = 0
     deleted = False
     state = State.OK
+    details = ""
 
     number_of_changes = len(section)
     if number_of_changes == 0:
         summary = "no pending changes"
     elif number_of_changes == 1:
-        max_time = section[0].get_time_in_seconds()
+        max_time = section[0].get_time_in_seconds
         if section[0].mode == "D":
             summary = str(number_of_changes) + " pending change"
             deleted = True
         else:
-            summary = f"{number_of_changes} pending change since {section[0].get_time_string()}"
+            summary = f"{number_of_changes} pending change since {section[0].get_time_string}"
+        details = section[0].__str__()
     else:
         max_time_string = ""
-        for lbu_index in range(len(section)):
-            seconds = section[lbu_index].get_time_in_seconds()
-            if section[lbu_index].mode != "D":
+        for lbu in section:
+            seconds = lbu.get_time_in_seconds
+            if lbu.mode != "D":
                 if max_time < seconds:
                     max_time = seconds
-                    max_time_string = section[lbu_index].get_time_string()
+                    max_time_string = lbu.get_time_string
             else:
                 deleted = True
+            details += f"{lbu.__str__()}\n"
         if deleted:
             summary = f"{number_of_changes} pending changes"
         else:
             summary = f"{number_of_changes} pending change since {max_time_string}"
 
     if deleted or crit <= max_time:
-        state = State.CRIT
+        state = params["critical_seconds"]
     elif warn <= max_time < crit:
-        state = State.WARN
+        state = params["warning_seconds"]
 
-    yield Result(state=state, summary=summary)
+    yield Result(state=state, summary=summary, details=details)
 
 
 def discovery_lbu_status(section: List[LBU]) -> InventoryResult:
@@ -105,17 +113,24 @@ def parse_lbu_status(string_table: StringTable) -> List[LBU]:
     return lbu_list
 
 
-register.agent_section(
+agent_section_lbu_status = AgentSection(
     name="lbu_status",
     parse_function=parse_lbu_status,
 )
 
-register.check_plugin(
+
+check_plugin_lbu_status = CheckPlugin(
     name="lbu_status",
+    sections=["lbu_status"],
     service_name="LBU Status",
     discovery_function=discovery_lbu_status,
     check_function=check_lbu_status,
-    check_ruleset_name="lbu_status",
-    check_default_parameters={"levels": (1800, 3600)}
+    check_ruleset_name="lbu_status_rules",
+    check_default_parameters={
+        "critical_seconds": 3600,
+        "critical_state": State.CRIT,
+        "warning_seconds": 1800,
+        "warning_state": State.WARN
+    }
 )
 
---
+++ rulsets/rulset_lbus_status.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from cmk.rulesets.v1 import Label, Title
+from cmk.rulesets.v1.form_specs import BooleanChoice, DefaultValue, DictElement, Dictionary, Integer, ServiceState
+from cmk.rulesets.v1.rule_specs import CheckParameters, HostAndItemCondition, Topic
+
+
+def _parameter_form_lbu_status():
+    return Dictionary(
+        elements = {
+            "warning_seconds": DictElement(
+                parameter_form = Integer(
+                    title = Title("Seconds until warning State"),
+                    prefill = DefaultValue(value=1800)
+                )
+            ),
+            "critical_seconds": DictElement(
+                parameter_form = Integer(
+                    title = Title("Seconds until critical State"),
+                    prefill = DefaultValue(value=3600)
+                )
+            ),
+            "warning_state": DictElement(
+                parameter_form = ServiceState(
+                    title = Title("Status when warning"),
+                    prefill = DefaultValue(value=ServiceState.CRIT)
+                )
+            ),
+            "critical_state": DictElement(
+                parameter_form = ServiceState(
+                    title = Title("Status when critical"),
+                    prefill = DefaultValue(value=ServiceState.CRIT)
+                )
+            )
+        }
+    )
+
+
+rule_spec_bind9 = CheckParameters(
+    name = "lbu_status_rules",
+    title = Title("LBU Status"),
+    topic = Topic.OPERATING_SYSTEM,
+    parameter_form = _parameter_form_lbu_status,
+    condition = HostAndItemCondition(item_title = Title("LBU Status"))
+)
